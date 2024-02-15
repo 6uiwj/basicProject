c97b4e5db0e50082cd82a72d219438a768a0366b
6uiwj <oterges00@gmail.com>
2024-02-16 00:00:38 +0900

    240216 json데이터, rest방식 에러페이지 정의, 에러페이지 공통템플릿

--- src/main/java/org/choongang/commons/rests/JSONData.java
+++ src/main/java/org/choongang/commons/rests/JSONData.java
@@ -1,4 +1,26 @@
 package org.choongang.commons.rests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
 //JSONData의 형식을 고정
-public class JSONData {
+
+/** 성공시 데이터는 많을 것임
+ * 이런 부분들은 setter를 통해 직접 하기보단
+ * 객체를 만들 때 직접 바로 할 수 있게 생성자 매개변수를 추가,
+ * 데이터가 없는 경우를 대비해 매개변수가 없는 생성자도 추가
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class JSONData<T> {
+    //성공 시가 더 많을 테니 성공 시 값을 고정
+
+    private HttpStatus status = HttpStatus.OK; //상태코드 가져오기
+    private boolean success = true; //성공여부
+    private T data; //성공시 데이터
+    private String message; //실패 시 메시지
+
 }
--- src/main/java/org/choongang/restcontrollers/RestCommonController.java
+++ src/main/java/org/choongang/restcontrollers/RestCommonController.java
@@ -1,8 +1,38 @@
 package org.choongang.restcontrollers;
 
+import org.choongang.commons.exceptions.CommonException;
+import org.choongang.commons.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 //레스트방식의 공통 에러처리
+//예외가 발생하며 유입될 수 있게 만들 것
+//형식은 json형식으로 데이터 통일
 @RestControllerAdvice("org.choongang.restcontrollers") //restcontrollers에 있는 모든 컨트롤러 범위
 public class RestCommonController {
+    //HTTP 응답을 나타내는 클래스
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<JSONData<Object>> errorHandler(Exception e) {
+        //공통 응답코드는 500으로 지정
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; //500
+
+        //발생한 에러가 CommonException의 객체이면 상태코드를 갖온다.
+        if (e instanceof CommonException) {
+            CommonException commonException = (CommonException)e;
+            status = commonException.getStatus();
+        }
+
+        //응답코드는 reponseEntity를 통해서 가져오고 JSON 데이터를 만든다?
+        JSONData<Object> data = new JSONData<>();
+        data.setSuccess(false); //실패시
+        data.setStatus(status); //상태코드가 바뀌므로 바꿔준다.
+        data.setMessage(e.getMessage()); //메시지도 ..
+
+        e.printStackTrace(); //콘솔에 에러 출력
+
+        //응답코드와 함꼐 바디를 통해 데이터 출력 - JSON 형태로 출력
+        return ResponseEntity.status(status).body(data);
+    }
 }
