51d7e08abd0a1d38bf5633bd409be9004e6330c9
6uiwj <oterges00@gmail.com>
2024-02-17 18:05:28 +0900

    240217 주석

--- src/main/java/org/choongang/commons/exceptions/CommonException.java
+++ src/main/java/org/choongang/commons/exceptions/CommonException.java
@@ -2,11 +2,12 @@ package org.choongang.commons.exceptions;
 
 import org.springframework.http.HttpStatus;
 //모든 예외를 정의해 줄 클래스
+//사용자가 정의한 예외 클래스가 RuntimeException의 하위 클래스임을 나타냄
 public class CommonException extends RuntimeException {
-    private HttpStatus status;
-
+    private HttpStatus status; //HTTP 응답코드 저장
+    //생성자
     public CommonException(String message, HttpStatus status) {
-        super(message);
+        super(message); //매개변수로 받은 message를 부모클래스인 RuntimeCxcption에 전달
         this.status = status;
     }
 
