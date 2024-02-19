09f596f3d401c2be668e594f4a8187fda7caeab3
6uiwj <oterges00@gmail.com>
2024-02-19 13:56:13 +0900

    240219 커맨드객체 컨트롤러 연결 및 회원가입 폼 작성 중

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,10 +1,13 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.choongang.commons.ExceptionProcessor;
 import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,17 +19,24 @@ public class MemberController implements ExceptionProcessor {
     private final Utils utils; //디바이스별 경로 설정을 위해
 
     //get방식으로 접속하면 회원가입 양식이 나오도록
-    @GetMapping("/join")
-    public String join() {
+    @GetMapping("/join") //RequestJoin
+    public String join(@ModelAttribute RequestJoin form) {
 
 
         //템플릿 연결
         return utils.tpl("member/join");
     }
+
     //회원가입 처리 - post방식 (join process)
+    //검증 필요 @Valid
     @PostMapping("/join")
-    public String joinPs() {
+    public String joinPs(@Valid RequestJoin form, Errors  errors) {
         //회원가입 성공시 로그인페이지로 이동
+        //에러가 발생하면 errors가 참 -> 다시 회원가입 템플릿을 보여주고 에러메시지 출력
+        if(errors.hasErrors()) {
+            return utils.tpl("member/join");
+        }
+
         return "redirect:/member/login";
     }
 
