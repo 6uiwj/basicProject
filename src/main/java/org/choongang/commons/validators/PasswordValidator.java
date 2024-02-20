9301bdfc9a3f37eee6cedc34fe5a35a8468dc832
6uiwj <oterges00@gmail.com>
2024-02-20 23:52:01 +0900

    240220 비밀번호 복잡성 만들기

--- src/main/java/org/choongang/commons/validators/PasswordValidator.java
+++ src/main/java/org/choongang/commons/validators/PasswordValidator.java
@@ -6,12 +6,23 @@ public interface PasswordValidator {
      * @param password
      * @param caseIncensitive
      *          false : 대문자 1개 이상, 소문자 1개 이상 포함해야 함
-     *          true: 대소문자 구분없이 1개 이상 포함
+     *          true: 대소문자 구분없이 1개 이상 포함 -> 따로 체크X
      * @return
      */
     default boolean alphaCheck(String password, boolean caseIncensitive) {
         //유연성을 주기 위해 대소문자 구분없이 체크, 대문자소문자 각각포함의 경우로 case 나눠주기
-    
+        if (caseIncensitive) { //대소문자 구분없이 체크
+            //나중에 정규표현식으로 바꿔주기
+            //1개이상의 대소문자를 포함하고 앞뒤로는 있어도 되고, 없어도 됨?
+            String pattern = ".*[a-zA-z]+.*";
+            return password.matches(pattern);
+
+        } else { //대문자 1개, 소문자 1개 포함
+            String pattern1 = ".*[a-z]+.*"; //소문자
+            String pattern2 = ".*[A-Z]+.*"; //대문자
+
+            return password.matches(pattern1) && password.matches(pattern2);
+        }
     }
 
     /**
@@ -20,7 +31,7 @@ public interface PasswordValidator {
      * @return
      */
     default boolean numberCheck(String password) {
-
+        return password.matches(".*\\d+.*"); // == .*[0-9]+.*
     }
 
     /**
@@ -29,7 +40,9 @@ public interface PasswordValidator {
      * @return
      */
     default boolean specialCharsCheck(String password){
-        
+        String pattern = ".*[`~!@#$%^*&()-_=]+.*";
+
+        return password.matches(pattern);
     }
 
 }
