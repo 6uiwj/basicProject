f6aa37d37ee83ed67b2eb341282c3517a9db039b
6uiwj <oterges00@gmail.com>
2024-02-21 22:22:38 +0900

    240221 비밀번호 해시화, 검증, 회원가입 처리

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.choongang.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 //스프링 시큐리티 설정 파일(스프링 시큐리티에 관한 설정은 전부 여기에 해줄 것임 )
@@ -14,4 +16,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build(); //기존 시큐리티 로그인 페이지 무력화
     }
+
+    /**
+     * BCycript를 이용한 비밀번호 해시화 - 스프링시큐리티 내장 기능 이용
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
