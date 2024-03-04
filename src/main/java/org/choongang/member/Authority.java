c850c600bd7d02cee8a5faaf0eea18ff240878de
6uiwj <oterges00@gmail.com>
2024-03-04 17:59:39 +0900

    240304 권한

--- src/main/java/org/choongang/member/Authority.java
+++ src/main/java/org/choongang/member/Authority.java
@@ -3,6 +3,6 @@ package org.choongang.member;
 public enum Authority {
     USER, //일반 사용자
     MANAGER, //부 관리자
-    ADMIN, //최고 관리자
+    ADMIN //최고 관리자
 
 }
--- src/main/java/org/choongang/member/entities/Authorities.java
+++ src/main/java/org/choongang/member/entities/Authorities.java
@@ -1,10 +1,28 @@
 package org.choongang.member.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.member.Authority;
 
 //권한은 상수로 설정
 @Data
 @Entity
+//회원 한명한테 동일한 권한을 여러개 부여할 수도 있으므로 권한과 멤버를 묶어서 unique 속성을 부여하자.
+@Table(indexes = @Index(name="uq_member_authority", columnList = "member_seq, authority",
+unique = true))
 public class Authorities {
+    @Id
+    @GeneratedValue
+    private Long seq; //기본키
+
+    //한 멤버가 여러가지의 권한을 받을 수 있다. -> ManyToOne관계(One:회원 - Many:권한)
+    // -> Authorities 쪽이 외래키
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "member_seq")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=15, nullable = false)
+    private Authority authority;
+
 }
