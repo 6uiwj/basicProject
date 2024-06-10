742d41e25ff2d6a4f343fd2dee23a546987ff3ef
6uiwj <oterges00@gmail.com>
2024-06-11 01:54:28 +0900

    240611 관리자페이지메뉴-기본설정

--- src/main/java/org/choongang/admin/config/repositories/ConfigRepository.java
+++ src/main/java/org/choongang/admin/config/repositories/ConfigRepository.java
@@ -3,5 +3,6 @@ package org.choongang.admin.config.repositories;
 import org.choongang.admin.config.entities.Configs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//1차원적인 쿼리만하기 사용할 것이기 때문에 queryDsl 상속X
 public interface ConfigRepository extends JpaRepository<Configs, String> {
 }
