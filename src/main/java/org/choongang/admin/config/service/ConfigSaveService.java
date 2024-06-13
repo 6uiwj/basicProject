e0958ab6cc2032bcce0421a7e395fc7cb103edd1
6uiwj <oterges00@gmail.com>
2024-06-13 23:53:33 +0900

    240613 관리자페이지메뉴 - 기본설정 서비스

---
+++ src/main/java/org/choongang/admin/config/service/ConfigSaveService.java
@@ -0,0 +1,10 @@
+package org.choongang.admin.config.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+//테이블에 저장하는 서비스(있으면 수정, 없으면 추가)
+@Service
+@RequiredArgsConstructor
+public class ConfigSaveService {
+}
